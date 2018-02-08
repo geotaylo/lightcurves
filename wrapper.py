b74a5741247e6e8c4516d9eb1aabd3c93be89ab1
geotaylo <u6253153@anu.edu.au>
2018-02-08 22:08:39 +1100

    fixed syntax

--- wrapper.py
+++ wrapper.py
@@ -16,8 +16,8 @@ nSNe = 100
 
 # Path to store info about whole observing set, used by each run.
 # ENSURE / is at end of path!
-parent_folder = 'Honours_data_sets/4_080218/1_stat_sample \
-                '/Kepler_6hours/SM_5day/vObs_2/100_SN/'
+parent_folder = 'Honours_data_sets/4_080218/1_stat_sample' \
+                '/Kepler_6hours/SM_5day/vObs_2/100SN/'
 
 
 # Paths to store individual runs
